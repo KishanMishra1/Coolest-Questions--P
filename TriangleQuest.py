b4b8cf916a0dd10bbad165b8de5cd7cef62acc52
Kishan Mishra <90112328+KishanMishra1@users.noreply.github.com>
2022-01-15 09:58:15 +0530

    Create TriangleQuest.py

---
+++ TriangleQuest.py
@@ -0,0 +1,11 @@
+CODE:
+for i in range(1,int(input())):
+    print((pow(10,i)//9)*i)
+        
+OUTPUT:
+1
+22
+333
+4444
+
+WHY TO DO HARD WORK THROUGH LOOPS WHEN YOU CAN JUST USE POWERFUL MATHS :P
