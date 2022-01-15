3389617fbcaceab974f088d6ef6d8f42e7726732
Kishan Mishra <90112328+KishanMishra1@users.noreply.github.com>
2022-01-15 10:20:32 +0530

    Update TriangleQuest.py

--- TriangleQuest.py
+++ TriangleQuest.py
@@ -1,11 +1,16 @@
-CODE:
-for i in range(1,int(input())):
-    print((pow(10,i)//9)*i)
-        
-OUTPUT:
+PROBLEM STATEMENT:
+
+#You are given a positive integer .Print a numerical triangle of height  like the one below:
+
 1
 22
 333
 4444
+55555
 
-WHY TO DO HARD WORK THROUGH LOOPS WHEN YOU CAN JUST USE POWERFUL MATHS :P
+
+CODE:
+for i in range(1,int(input())):
+    print((pow(10,i)//9)*i)
+
+WHY TO DO HARD WORK TH LOOPS WHEN YOU CAN JUST USE LORD MATHS :P
